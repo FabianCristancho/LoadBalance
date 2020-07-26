60f032445c45f287a9fc84fb34a193d1b70bdb3c
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-07-25 23:09:06 -0500

    lab1 config all functionality

---
+++ Test2/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const multer = require('multer');
+const bodyparser = require('body-parser');
+const fs = require('fs');
+const du = require('du');
+const { Console } = require('console');
+const port = 8084;
+
+app.get('/', (req, res) => {
+     res.send('Servidor activo');
+     //let size = await du(__dirname + '/public/uploads');
+     let limit = 9.5e7;
+     console.log(limit);
+     //console.log(`The size of /home/rvagg/.npm/ is: ${size} bytes`);
+});
+
+const storage = multer.diskStorage({
+     destination: path.join(__dirname, 'public/uploads'),
+     filename: (req, file, cb) => {
+          cb(null, file.originalname);
+     }
+});
+
+app.use(multer({
+     storage,
+     dest: path.join(__dirname, 'public/uploads')
+ }).single('image'))
+
+app.use(express.static(__dirname + '/public/uploads'));
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+
+app.post("/", async (request, response) => {
+     console.log('--------------Server has heard---------------');
+     console.log(request.file );
+     let size = await du(__dirname + '/public/uploads');
+     console.log("Size is: " +size);
+     response.send("" +size);
+ });
+
+ app.get("/sizeFolder", async (req, res) => {
+     let size = await du(__dirname + '/public/uploads');
+     console.log("Size is: " +size);
+     res.send("" +size);
+ });
+
+
+ //METODO ESTA SIRVIENDO BIEN
+// app.post("/", (request, response) => {
+//      console.log('--------------Server has heard---------------');
+//      console.log('My name is ' +request.body.firstName +" " +request.body.lastName +", i am " +request.body.age +" years old, and i live in " +request.body.city);
+//      response.send('Received data');
+//  });
+
+// const storage = multer.diskStorage({
+//      destination: path.join(__dirname, '../public/uploads'),
+//      filename: (req, file, cb) => {
+//          cb(null, file.originalname);
+//      }
+//  });
+
+ //settings
+// app.set('port', process.env.PORT || 8081);
+
+
+
+//  app.post('/', (req, res) => {
+//      console.log('Proceso Exitoso');
+//      console.log(req.file);
+// });
+
+app.listen(port, function(){
+     console.log('Servidor funcionando en el puerto ' +port);
+});
--- serverUser/src/routes/index.js
+++ serverUser/src/routes/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const dbConnection = require('../db/dbConnection');
 const axios = require('axios');
 const formData = require('form-data');
 const fs = require('fs');
+const connection = dbConnection();
+var pathImage = "";
 
 const router = express.Router();
+
 router.get('/', (req,res) =>{
-     res.render('index');
+     connection.query("SELECT * FROM images", (err, rows)=>{
+          if(err) throw err;
+          res.render('index' ,{rows, pathImage});
+     });
 });
+
+router.get('/image', (req,res) => {
+     console.log("entra");
+     console.log(req.query);
+     pathImage = req.query.server +"/" +req.query.code;
+     console.log(pathImage);
+     res.redirect('/');
+})
 
 router.post('/upload', (req, res) => {
      if(req.file == null){
@@ -14,9 +29,12 @@ router.post('/upload', (req, res) => {
      }else{
           console.log("Nombre de archivo");
           console.log(req.file.filename);
+          console.log("Tamaño imagen: " +req.file.size);
+          console.log("Nombre original");
+          console.log(req.file.originalname);
+          originalName = req.file.originalname;
           nameImage = req.file.filename;
           const form = new formData();
-
           /*
           soliciteSizeFile(8081).then((res) => {
                console.log("Tam1: " +res);
@@ -26,6 +44,40 @@ router.post('/upload', (req, res) => {
                console.log("Tam server1: " +res[0]);
                console.log("Tam server2: " +res[1]);
                console.log("Tam server3: " +res[2]);
+               let lowerSize = getLowerSize(res[0], res[1], res[2]);
+               let serverLower = getLowerServer(res[0], res[1], res[2]);
+
+               if(lowerSize < 9.5e7){
+                    form.append('image', fs.createReadStream(req.file.path));
+                    // La url para contenedores es: 'http://files:8081/'
+                    axios.post('http://localhost:'+serverLower, form, {
+                         headers: {
+                              'Content-Type': `multipart/form-data; boundary=${form._boundary}`
+                         }
+                    }).then(function (response) {
+                         console.log('La imagen se ha enviado de forma satisfactoria');
+
+                         connection.query("INSERT INTO images(original_name, code_name, size,server) VALUES ('"+req.file.originalname +"','" +req.file.filename +"'," +req.file.size +",'" +serverLower +"')", (err, result) => {
+                              if(result){
+                                   console.log('Los datos de la donacion se han agregado correctamente');
+                              }else{
+                                   console.log('Ha ocurrido un problema al insertar los datos de la donacion')
+                                   console.log(err);
+                              }
+                         });
+
+                         console.log(response.data);
+                    })
+                    .catch(function (response) {
+                         console.log(response.data);
+                    }); 
+                    fs.unlink(req.file.path, (err) => {
+                         if(err) throw err;
+                         console.log('Limpiando el servidor');
+                    });
+               }else{
+                    console.log("Se ha sobrepasado las 10MB");
+               }
           })
 
           /*
@@ -80,10 +132,10 @@ function getSizeServersFolder(){
           soliciteSizeFile(8081).then((res) => {
                array.push(res);
           }).then((res) => {
-	       soliciteSizeFile(8082).then((res) => {
+	       soliciteSizeFile(8083).then((res) => {
                	 array.push(res);
                }).then((res) => {
-                    soliciteSizeFile(8083).then((res) => {
+                    soliciteSizeFile(8084).then((res) => {
                          array.push(res);
                    }).then((res) => {
                         resolve(array);
@@ -93,13 +145,22 @@ function getSizeServersFolder(){
      });
 }
 
-function getLowerSize(sizeServer1, sizeServer2, sizeServer3){
+function getLowerServer(sizeServer1, sizeServer2, sizeServer3){
      if(sizeServer1<sizeServer2 && sizeServer1<sizeServer3){
           return "8081";
      }else if(sizeServer2<sizeServer3){
-          return "8082";
+          return "8083";
      }
-     return "8083";
+     return "8084";
+}
+
+function getLowerSize(sizeServer1, sizeServer2, sizeServer3){
+     if(sizeServer1<sizeServer2 && sizeServer1<sizeServer3){
+          return sizeServer1;
+     }else if(sizeServer2<sizeServer3){
+          return sizeServer2;
+     }
+     return sizeServer3;
 }
 
 module.exports = router;
